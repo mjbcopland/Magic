c1f3c470d9e712ef16f46ec38ce0dccd5b1e155a
JamesJApicella <james.j.apicella@gmail.com>
2019-08-23 16:53:54 +0100

    Reeeee

--- James_code/Elements.py
+++ James_code/Elements.py
@@ -5,22 +5,49 @@ class Elements:
         self.velocity = velocity
         self.shape = shape
 
+    def interact_on(self, state):
+        return state
+
+    def interact_from(self, state):
+        pass
+
 
 class Fire(Elements):
     def __init__(self, temperature, position, velocity, shape):
         Elements.__init__(self, position, velocity, shape)
         self.temperature = temperature
 
+    def interact_on(self, state):
+        state["Temperature"] = (state["Temperature"] + self.temperature)/2
+        return state
+
+    def interact_from(self, state):
+        self.temperature = state["Temperature"]
+        return self.check_status()
+
+    def check_status(self):
+        if self.temperature < 300:
+            return False
+        else:
+            return True
 
 class Water(Elements):
     def __init__(self, temperature, position, velocity, shape):
         Elements.__init__(self, position, velocity, shape)
         self.temperature = temperature
 
+    def interact_on(self, state):
+        state["Temperature"] = (state["Temperature"] + self.temperature)/2
+        return state
+
+    def interact_from(self, state):
+        self.temperature = state["Temperature"]
+        return True
+
     def check_status(self):
         if self.temperature < 0:
-            return print('Ice')
+            return True
         elif self.temperature > 100:
-            return print('Steam')
+            return True
         else:
-            return print('Water')
+            return True
--- James_code/World.py
+++ James_code/World.py
@@ -1,8 +1,32 @@
 from People import *
 
+
+class Tile:
+    def __init__(self, coordinates):
+        self.coordinates = coordinates
+        self.objects = []
+        self.state = {"Temperature": 24, "Time": 1, "Gravity": 9.81}
+
+    def resolve_tile(self):
+        for object in self.objects:
+            self.state = object.interact_on(self.state)
+        for object in self.objects:
+            if not object.interact_from(self.state):
+                self.objects.remove(object)
+
+
+    def add_object(self, object):
+        self.objects.append(object)
+
+
+tile_1 = Tile((0,0))
 Bob = Wizard((0,0), 0)
 spell_effect = Bob.shout('Fire 5 Create Cone Point 5')
 
 Carl = Wizard((0,0), 0)
 spell_effect_2 = Carl.shout('Cold 3 Create Cone Point 5')
-spell_effect_2.check_status()
+
+tile_1.add_object(spell_effect)
+tile_1.add_object(spell_effect_2)
+tile_1.resolve_tile()
+print('hi')

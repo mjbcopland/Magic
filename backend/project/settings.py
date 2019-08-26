306c6ab3887e1dd5353918d68cbeb9b5c474a567
Michael Copland <mjbcopland@gmail.com>
2019-08-26 10:21:26 +0100

    Rename project

--- backend/project/settings.py
+++ backend/project/settings.py
@@ -52,7 +52,7 @@ MIDDLEWARE = [
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
 
-ROOT_URLCONF = "magic.urls"
+ROOT_URLCONF = "project.urls"
 
 TEMPLATES = [
     {
@@ -70,7 +70,7 @@ TEMPLATES = [
     }
 ]
 
-WSGI_APPLICATION = "magic.wsgi.application"
+WSGI_APPLICATION = "project.wsgi.application"
 
 
 # Database
